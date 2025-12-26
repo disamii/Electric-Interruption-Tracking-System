9f87aff0f582d6ce49287070626a9a405dea2ea8
samson mamuye <samsonmamuye360@gmail.com>
2025-12-26 20:29:36 +0300

    docker fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    server: {
+    host: true,  // bind to 0.0.0.0
+    port: 5173
+  },
   plugins: [react()],
 })
 
